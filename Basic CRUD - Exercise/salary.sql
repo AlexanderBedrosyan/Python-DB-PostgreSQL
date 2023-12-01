9daee7ccc4e2e5e1aff3633acd712f82c33c0f74
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-02 01:14:30 +0200

    Solutions for Basic CRUD - Exercise
    Solutions for Basic CRUD - Exercise

---
+++ Basic CRUD - Exercise/salary.sql
@@ -0,0 +1,10 @@
+SELECT
+    CONCAT_WS(' ', first_name, last_name) AS "full_name",
+    job_title,
+    salary
+FROM
+    employees
+WHERE
+    salary in (12500, 14000, 23600, 25000)
+ORDER BY
+    salary DESC;
