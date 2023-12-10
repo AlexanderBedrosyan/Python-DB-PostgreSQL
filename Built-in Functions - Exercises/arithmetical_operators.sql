319a86894b3230a8a52648ca9a11f32e952c2c5b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-11 00:29:09 +0200

    Solutions for Built-in Functions - Exercises
    Solutions for Built-in Functions - Exercises

---
+++ Built-in Functions - Exercises/arithmetical_operators.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "bookings_calculation" AS
+    SELECT
+        booked_for
+    FROM
+        bookings
+    WHERE
+        apartment_id = 93;
+
+ALTER TABLE
+    bookings_calculation
+ADD COLUMN
+    "multiplication" NUMERIC,
+ADD COLUMN
+    "modulo" NUMERIC;
+
+UPDATE bookings_calculation
+SET
+    multiplication = booked_for * 50,
+    modulo = booked_for % 50;
