bfee370ed6da6f75da0026cbf1648098f0fd882b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-23 14:06:10 +0200

    Solutions for Data Aggregation - Exercise
    Solutions for Data Aggregation - Exercise

---
+++ Data Aggregation - Exercise/categorizes_salary.sql
@@ -0,0 +1,13 @@
+SELECT
+    job_title,
+    CASE
+        WHEN AVG(salary) > 45800 THEN 'Good'
+        WHEN AVG(salary) BETWEEN 27500 AND 45800 THEN 'Medium'
+        WHEN AVG(salary) < 27500 THEN 'Need Improvement'
+    END AS category
+FROM
+    employees
+GROUP BY
+    job_title
+ORDER BY
+    category, job_title;
