8dd2076726b18f4c6ced6c0a17d4a83a1a3f4ecf
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-12-30 00:33:47 +0200

    Solutions for Table Relations - Exercise
    Solutions for Table Relations - Exercise

---
+++ Table Relations - Exercise/online_store.sql
@@ -0,0 +1,54 @@
+CREATE TABLE item_types(
+    id SERIAL PRIMARY KEY ,
+    item_type_name VARCHAR
+);
+
+CREATE TABLE items(
+    id SERIAL PRIMARY KEY ,
+    item_name VARCHAR,
+    item_type_id INT,
+    CONSTRAINT fk_items_item_types
+                   FOREIGN KEY (item_type_id)
+                   REFERENCES item_types(id)
+                   ON DELETE CASCADE
+);
+
+CREATE TABLE cities(
+    id SERIAL PRIMARY KEY ,
+    city_name VARCHAR
+);
+
+CREATE TABLE customers(
+    id SERIAL PRIMARY KEY ,
+    customer_name VARCHAR,
+    birthday DATE,
+    city_id INT,
+    CONSTRAINT fk_customers_cities
+                      FOREIGN KEY (city_id)
+                      REFERENCES cities(id)
+                      ON DELETE CASCADE
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY ,
+    customer_id INT,
+    CONSTRAINT fk_orders_customers
+                   FOREIGN KEY (customer_id)
+                   REFERENCES customers(id)
+                   ON DELETE CASCADE
+);
+
+CREATE TABLE order_items(
+    order_id INT,
+    item_id INT,
+    CONSTRAINT pk_order_items
+                        PRIMARY KEY (order_id, item_id),
+    CONSTRAINT fk_order_items_orders
+                        FOREIGN KEY (order_id)
+                        REFERENCES orders(id)
+                        ON DELETE CASCADE ,
+    CONSTRAINT fk_order_items_items
+                        FOREIGN KEY (item_id)
+                        REFERENCES items(id)
+                        ON DELETE CASCADE
+)
