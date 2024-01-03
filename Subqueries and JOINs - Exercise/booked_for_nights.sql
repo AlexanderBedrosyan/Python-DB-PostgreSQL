8d381f76398fe16fe032c32e3ed97a2b53fbb593
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-01-03 08:58:23 +0200

    Solutions for Subqueries and JOINs - Exercise
    Solutions for Subqueries and JOINs - Exercise

---
+++ Subqueries and JOINs - Exercise/booked_for_nights.sql
@@ -0,0 +1,9 @@
+SELECT
+    CONCAT(a.address, ' ', a.address_2) as "apartment_address",
+    b.booked_for as nights
+FROM
+    apartments as a
+        JOIN bookings as b
+        ON a.booking_id = b.booking_id
+ORDER BY
+    a.apartment_id
