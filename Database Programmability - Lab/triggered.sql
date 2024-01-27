9b5b3d9f0fd2d2fe4045c2b980303f83d15807b3
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-01-27 10:36:01 +0200

    Solutions for Database Programmability - Lab
    Solutions for Database Programmability - Lab

---
+++ Database Programmability - Lab/triggered.sql
@@ -0,0 +1,41 @@
+CREATE TABLE deleted_employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    middle_name VARCHAR(20),
+    job_title VARCHAR(50),
+    department_id INT,
+    salary NUMERIC(19, 4)
+);
+
+CREATE OR REPLACE FUNCTION backup_fired_employees()
+RETURNS TRIGGER
+AS
+$$
+BEGIN
+    INSERT INTO deleted_employees(
+        first_name,
+        last_name,
+        middle_name,
+        job_title,
+        department_id,
+        salary
+    )
+    VALUES (
+        OLD.first_name,
+        OLD.last_name,
+        OLD.middle_name,
+        OLD.job_title,
+        OLD.department_id,
+        OLD.salary
+    );
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER backup_employees
+AFTER DELETE ON employees
+FOR EACH  ROW
+EXECUTE PROCEDURE backup_fired_employees();
