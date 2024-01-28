50238a0f0f96507acf21a4eca01ade630fc70077
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-01-28 10:41:12 +0200

    Solutions for Functions and Procedures - Exercise
    Solutions for Functions and Procedures - Exercise

---
+++ Functions and Procedures - Exercise/game_over.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION fn_is_game_over(is_game_over BOOLEAN)
+RETURNS TABLE(
+    name VARCHAR(50),
+    game_type_id INT,
+    is_finished BOOLEAN
+             )
+AS
+    $$
+    BEGIN
+        RETURN QUERY
+        WITH needed_table AS (
+            SELECT
+                g.name,
+                g.game_type_id,
+                g.is_finished
+                FROM games AS g
+            WHERE
+                g.is_finished = is_game_over
+            )
+        SELECT * FROM needed_table;
+    END;
+    $$
+LANGUAGE plpgsql;
