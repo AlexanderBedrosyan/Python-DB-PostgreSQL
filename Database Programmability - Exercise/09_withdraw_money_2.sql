71c6ffe5c0fbd3993a3104659afe7b1a455ea917
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-06-10 00:41:08 +0300

    Add solution for 09 part 2
    Add solution for 09 part 2

---
+++ Database Programmability - Exercise/09_withdraw_money_2.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE sp_withdraw_money(account_id INT, money_amount NUMERIC)
+LANGUAGE plpgsql
+AS
+    $$
+    DECLARE
+        current_amount NUMERIC;
+    BEGIN
+        current_amount := (
+        SELECT
+            balance
+        FROM
+            accounts
+        WHERE
+            id = account_id);
+        IF current_amount - money_amount >= 0 THEN
+            UPDATE accounts
+            SET balance = balance - money_amount
+            WHERE id = account_id;
+        ELSE
+            RAISE NOTICE 'Insufficient balance to withdraw %', money_amount;
+        end if;
