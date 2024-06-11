805b51dcd3b6c728b191890d15bed4c1f3f9b442
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-06-12 00:14:25 +0300

    Add solution 01
    Add solution 01

---
+++ exam_preparation_I/1_1_database_design.sql
@@ -0,0 +1,37 @@
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(50)
+);
+CREATE TABLE animal_types(
+    id SERIAL PRIMARY KEY,
+    animal_type VARCHAR(30) NOT NULL
+);
+CREATE TABLE cages(
+    id SERIAL PRIMARY KEY,
+    animal_type_id INTEGER REFERENCES animal_types(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+CREATE TABLE animals(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    birthdate DATE NOT NULL,
+    owner_id INTEGER REFERENCES owners(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    animal_type_id INTEGER REFERENCES animal_types(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+CREATE TABLE volunteers_departments(
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+CREATE TABLE volunteers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(50),
+    animal_id INTEGER REFERENCES animals(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    department_id INTEGER REFERENCES volunteers_departments(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+CREATE TABLE animals_cages(
+    cage_id INTEGER REFERENCES cages ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    animal_id INTEGER REFERENCES animals(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL 
+);
