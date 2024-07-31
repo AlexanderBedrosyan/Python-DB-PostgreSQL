a3526c83a937b2dd27f096db8e19e84a4117be6a
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-31 17:18:25 +0300

    Add solution for 2.4
    Add solution for 2.4

---
+++ exam_preparation_III/2_4_delete.sql
@@ -0,0 +1,21 @@
+DELETE FROM board_games
+WHERE publisher_id IN (
+    SELECT id
+    FROM publishers
+    WHERE address_id IN (
+        SELECT id
+        FROM addresses
+        WHERE town LIKE 'L%'
+    )
+);
+DELETE FROM publishers
+WHERE address_id = (
+    SELECT
+        id
+    FROM
+        addresses
+    WHERE
+        town LIKE 'L%'
+    );
+DELETE FROM addresses
+WHERE town LIKE 'L%';
