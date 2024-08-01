ee85b06a94279f700a774dbe71adcc6935113146
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-01 08:40:57 +0300

    Add solution for 3.6
    Add solution for 3.6

---
+++ exam_preparation_III/3_6_board_games_by_category.sql
@@ -0,0 +1,16 @@
+SELECT
+    bg.id,
+    bg.name,
+    bg.release_year,
+    c.name
+FROM
+    board_games AS bg
+JOIN
+    categories AS c
+ON
+    bg.category_id = c.id
+WHERE
+    c.name IN ('Strategy Games', 'Wargames')
+ORDER BY
+    bg.release_year DESC
+;
