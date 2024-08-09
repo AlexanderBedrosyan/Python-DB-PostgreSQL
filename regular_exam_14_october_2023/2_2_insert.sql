4d8e052a2f6eb556fd1013c078910b05469d8208
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-09 08:55:45 +0300

    Add solution for 02 02
    Add solution for 02 02

---
+++ regular_exam_14_october_2023/2_2_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO coaches(first_name, last_name, salary, coach_level)
+SELECT
+   first_name,
+   last_name,
+   salary * 2,
+   LENGTH(first_name)
+FROM
+   players
+WHERE
+   hire_date < '2013-12-13 07:18:46';
