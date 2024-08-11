89359ed83ed196214241cbcda36f5676b4f566a1
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-11 11:11:24 +0300

    Add solution for 03 07
    Add solution for 03 07

---
+++ regular_exam_14_october_2023/3_7_teams_with_player_count_and_fan_base.sql
@@ -0,0 +1,17 @@
+SELECT
+    t.id,
+    t.name,
+    COUNT(p.id) AS player_count,
+    t.fan_base
+FROM
+    teams AS t
+LEFT JOIN
+    players AS p
+ON
+    t.id = p.team_id
+WHERE
+    t.fan_base > 30000
+GROUP BY
+    t.id, t.name, t.fan_base
+ORDER BY
+    player_count desc, t.fan_base DESC;
